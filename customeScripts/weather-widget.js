9f62fab5141787d66ecfa0b3f44af75f98d9eae5
carlosfabianchaves <48218393+carlosfabianchaves@users.noreply.github.com>
2020-04-13 17:10:36 -0600

    Add files via upload

---
+++ customeScripts/weather-widget.js
@@ -0,0 +1,21 @@
+
+
+setTimeout(()=>{
+    document.getElementById("plusSidebarWidget").addEventListener("click", function(){
+        this.classList.toggle("open");
+        if(this.classList.contains("open")){
+            this.setAttribute("src", "./images/icons/close-icon.svg");
+        }else{
+            this.setAttribute("src", "./images/icons/plus-icon.svg");
+        }
+        var widgetWeatherElement = document.getElementById("widgetWeather");
+        widgetWeatherElement.classList.toggle("show-widget");
+        var parentElemnt = widgetWeatherElement.parentElement;
+        var sidebarElements = Array.from(parentElemnt.getElementsByClassName("hide-mobile"));          
+        sidebarElements.forEach(function(item){
+            item.classList.toggle("hide");
+        });
+        document.getElementsByClassName("sidebar-widget")[0].classList.toggle("mobile-active");           
+    
+    });
+},250);
---
+++ precache-manifest.4bfd32d3ebc7e8e0f96816c8e2330893.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "383a1012317bd884885cdb86886920a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b23d708ebe297edfff4d",
+    "url": "/static/css/main.88640a17.chunk.css"
+  },
+  {
+    "revision": "cad122e955933e0e9605",
+    "url": "/static/js/2.6ecd98f4.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/static/js/2.6ecd98f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b23d708ebe297edfff4d",
+    "url": "/static/js/main.ae48a402.chunk.js"
+  },
+  {
+    "revision": "e7577de7972d1c3b21d8",
+    "url": "/static/js/runtime-main.d4756f21.js"
+  },
+  {
+    "revision": "1053dab831ed7a48a67ff7771ea7ccd9",
+    "url": "/static/media/hero-home.1053dab8.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
